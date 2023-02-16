98b2a965eb5f76490dbbe6d905f0be5a00eb271e
wanning-lu <wanning.lu@gmail.com>
2023-02-16 15:50:21 -0800

    made the bash script + edited test file

--- TestListExamples.java
+++ TestListExamples.java
@@ -2,6 +2,7 @@ import static org.junit.Assert.*;
 import org.junit.*;
 import java.util.Arrays;
 import java.util.List;
+import java.util.ArrayList;
 
 class IsMoon implements StringChecker {
   public boolean checkString(String s) {
@@ -17,5 +18,22 @@ public class TestListExamples {
     List<String> merged = ListExamples.merge(left, right);
     List<String> expected = Arrays.asList("a", "a", "b", "c", "d");
     assertEquals(expected, merged);
+  }
+
+  @Test
+  public void testFilter() {
+      ArrayList<String> input = new ArrayList<>();
+      input.add("Moon");
+      input.add("cherry");
+      input.add("moon");
+      input.add("d");
+      input.add("moot");
+      input.add("bc");
+
+      List<String> expectedResult = new ArrayList<>();
+      StringChecker sc = new IsMoon();
+      expectedResult.add("Moon");
+      expectedResult.add("moon");
+      assertEquals(expectedResult, ListExamples.filter(input, sc));
   }
 }
--- grade.sh
+++ grade.sh
@@ -1,5 +1,42 @@
-CPATH='.:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar'
+CPATH='.;lib/hamcrest-core-1.3.jar;lib/junit-4.13.2.jar'
 
 rm -rf student-submission
+rm -rf testing
 git clone $1 student-submission
-echo 'Finished cloning'
+echo "Finished cloning"
+
+cd student-submission
+if [[ -f ListExamples.java ]]
+then
+    echo "File found"
+else
+    echo "File not found, try changing the name of the file to ListExamples"
+    exit 1
+fi
+
+cd ..
+mkdir testing
+cp student-submission/ListExamples.java TestListExamples.java testing
+cd testing
+mkdir lib
+cp ../lib/hamcrest-core-1.3.jar ../lib/junit-4.13.2.jar lib
+
+ls
+javac -cp "$CPATH" *.java
+if [[ $? -ne 0 ]]
+then
+    echo "Failed compilation, please check your file for errors!"
+    exit 1
+else
+    echo "Compilation success!"
+fi
+
+java -cp "$CPATH" org.junit.runner.JUnitCore TestListExamples > results.txt
+PASSCOUNT=`grep -c "OK" results.txt`
+
+if [[ $PASSCOUNT -eq 1 ]]
+then
+    echo "Congrats, you passed! A for you!"
+else 
+    echo "You are a complete failure. You get an F for failing all these tests."
+fi
