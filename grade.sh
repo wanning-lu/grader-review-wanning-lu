7aa8d3b4f29e5fa5baaed27f280630380fd836ed
wanning-lu <wanning.lu@gmail.com>
2023-03-02 15:07:19 -0800

    i fixed everything

--- grade.sh
+++ grade.sh
@@ -1,7 +1,9 @@
-CPATH='.:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar'
+LINUX_CPATH='.:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar'
+WINDOWS_CPATH='.;lib/hamcrest-core-1.3.jar;lib/junit-4.13.2.jar'
 
 rm -rf student-submission
 rm -rf testing
+rm -rf results.txt
 
 git clone $1 student-submission
 echo "Finished cloning"
@@ -22,7 +24,13 @@ cd testing
 mkdir lib
 cp ../lib/hamcrest-core-1.3.jar ../lib/junit-4.13.2.jar lib
 
-javac -cp $CPATH *.java
+if [[ "$OSTYPE" == "linux-gnu"* || "$OSTYPE" == "darwin"* ]]
+then
+    javac -cp $LINUX_CPATH *.java
+else
+    javac -cp "$WINDOWS_CPATH" *.java
+fi
+
 if [[ $? -ne 0 ]]
 then
     echo "Failed compilation, please check your file for errors!"
@@ -31,12 +39,20 @@ else
     echo "Compilation success!"
 fi
 
-java -cp $CPATH org.junit.runner.JUnitCore TestListExamples > results.txt
-PASSCOUNT=`grep -c "OK" results.txt`
-
-if [[ $PASSCOUNT -eq 1 ]]
+if [[ "$OSTYPE" == "linux-gnu"* || "$OSTYPE" == "darwin"* ]]
 then
-    echo "Congrats, you passed! A for you!"
-else 
-    echo "You did not pass all of the tests. Please check your code."
+    java -cp $LINUX_CPATH org.junit.runner.JUnitCore TestListExamples > results.txt
+else
+    java -cp "$WINDOWS_CPATH" org.junit.runner.JUnitCore TestListExamples > results.txt
+fi
+
+tail -3 results.txt > check.txt
+grep -q "FAILURES" check.txt
+if [[ $? -eq 0 ]]
+then
+    testsFailed=`grep -w "Tests" check.txt`
+    lastCharacter=${testsFailed: -1}
+    echo "You only passed ${lastCharacter} out of 6 tests :("
+else
+    echo "You passed all the tests! Congrats!"
 fi
